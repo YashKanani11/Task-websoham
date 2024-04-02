3cbff0aaee35f42255d32bea95e35b4c44b56b63
Yash Kanani <ykkanani.yk@gmail.com>
2024-04-02 10:35:35 +0530

    I dont know what i have changed

--- my-project/src/BlogCategories.jsx
+++ my-project/src/BlogCategories.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import SectionHeaders from './CommonComponents/SectionHeaders'
+import Slider3d from './CommonComponents/_3DCarousel'
 
 const BlogCategories = () => {
   return (
     <>
     <section className='h-screen w-full font-[Archivo] bg-black rounded-t-[150px] mt-10 text-white pt-10 px-20 relative overflow-hidden'>
       <SectionHeaders mainHead1={'Blogs'} mainHead2={'Categories'} subHead={'our top categories'} para={'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'} accentColor={'#c3df5c'} mainColor='#ffffff'/>
+      <div>
+        <Slider3d/>
+      </div>
       {/* Absolute design elemnts */}
       <div className='absolute top-[45%] -right-[88px] z-20'>
         <img className='h-44 bg-blend-color-dodge' src='https://s3-alpha-sig.figma.com/img/4bbe/1075/2ff10844f801484df03a6101ed8bdc0a?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eMxD2~tjYs8J1mWS2I-DYEG2~KoJrch5k2k3H-IzLfqu1rIRQVEwZRX2p9uC9A29olwWQgD~Ob7RdMuQemTN7papIsjGpHKPSpAeRZtEIvn-WlGOhD3NlEMfjMq7DC~JYT-mVk0XRFBup16FIDxlgJ6A1Y20pgUkOuJA1QmpiiNiS486~VRw3FG4tzfpHZBspHd~cH6ptN5lG6dyF~LmQAzk2na-Hdliz1NlxpvnDflAax-hqrr~OqgO1YcN-c5MRLh7NK4C1L04nJhN93Aa6p42z5bnxBhdi6Pyj-B~F592fmeAQq86PcmZr-T98wJYx3LEvsTdyy~F1t8N0W0LEg__'/>
--- my-project/src/CommonComponents/_3DCarousel.jsx
+++ my-project/src/CommonComponents/_3DCarousel.jsx
@@ -1,39 +1,120 @@
 import React, { useState } from 'react';
-import './Carousel.css';
+function Slider3d() {
+  const [backfaceVisibility, setBackfaceVisibility] = useState(true);
+  const [overflow, setOverflow] = useState(true);
+  const [perspectiveOriginX, setPerspectiveOriginX] = useState(50);
+  const [perspectiveOriginY, setPerspectiveOriginY] = useState(50);
+  const [perspective, setPerspective] = useState(800);
+  const [slideAmount, setSlideAmount] = useState(6);
 
-function Carousel() {
-  const [currdeg, setCurrDeg] = useState(0);
-  const [front,setfront]=useState(1)
-  const rotate = (direction) => {
-    if (direction === "n") {
-      setCurrDeg(currdeg - 60);
-      setfront(front+1)
-      alert(front)
-    } else if (direction === "p") {
-      setCurrDeg(currdeg + 60);
-    }
+  const handleBackfaceVisibility = (event) => {
+    setBackfaceVisibility(event.target.checked);
+  };
+
+  const handleOverflow = (event) => {
+    setOverflow(event.target.checked);
+  };
+
+  const handlePerspectiveOriginX = (event) => {
+    setPerspectiveOriginX(event.target.value);
+  };
+
+  const handlePerspectiveOriginY = (event) => {
+    setPerspectiveOriginY(event.target.value);
+  };
+
+  const handlePerspective = (event) => {
+    setPerspective(event.target.value);
+  };
+
+  const handleSlideRemove = () => {
+    if (slideAmount < 4) return;
+    setSlideAmount(slideAmount - 1);
+  };
+
+  const handleSlideAdd = () => {
+    setSlideAmount(slideAmount + 1);
   };
 
   return (
-    <div className="container">
-      <div className="carousel"
-      style={{
-        "-webkit-transform": `rotateY(${currdeg}deg)`,
-        "-moz-transform": `rotateY(${currdeg}deg)`,
-        "-o-transform": `rotateY(${currdeg}deg)`,
-        "transform": `rotateY(${currdeg}deg)`
-      }}>
-        <div className="item a 1">A</div>
-        <div className="item b 2">B</div>
-        <div className="item c 3">C</div>
-        <div className="item d 4">D</div>
-        <div className="item e 5">E</div>
-        <div className="item f 6">F</div>
+    <div>
+      <div className="slider3d">
+        <div className="slider3d_wrap">
+          {/* Render slideAmount number of divs */}
+          {[...Array(slideAmount)].map((_, index) => (
+            <div key={index}>{index + 1}</div>
+          ))}
+        </div>
       </div>
-      <div className="next" onClick={() => rotate("n")}>Next</div>
-      <div className="prev" onClick={() => rotate("p")}>Prev</div>
+      <div className="slider3d_left">&larr;</div>
+      <div className="slider3d_right">&rarr;</div>
+
+      <div className="ctrl">
+        <div className="visibility">
+          <label>
+            <input
+              type="checkbox"
+              checked={backfaceVisibility}
+              onChange={handleBackfaceVisibility}
+            />
+            backface-visibility
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={overflow}
+              onChange={handleOverflow}
+            />
+            overflow
+          </label>
+        </div>
+
+        <div className="perspOrig">
+          <p>perspective-origin:</p>
+          <input
+            type="number"
+            value={perspectiveOriginX}
+            min="-200"
+            max="200"
+            step="10"
+            onChange={handlePerspectiveOriginX}
+          />
+          <span>%</span>
+          <input
+            type="number"
+            value={perspectiveOriginY}
+            min="-200"
+            max="200"
+            step="10"
+            onChange={handlePerspectiveOriginY}
+          />
+          <span>%</span>
+        </div>
+
+        <div className="slideAmount">
+          <span className="slideRem" onClick={handleSlideRemove}>
+            -
+          </span>
+          <span>slide</span>
+          <span className="slideAdd" onClick={handleSlideAdd}>
+            +
+          </span>
+        </div>
+
+        <div className="perspValue">
+          <p>Perspective</p>
+          <input
+            type="range"
+            value={perspective}
+            min="10"
+            max="1000"
+            step="10"
+            onChange={handlePerspective}
+          />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Carousel;
+export default Slider3d;
